e2c282b7e012353b7d2e11ae2ae04389952ffd15
Douglas Vinicius Dierings <146199335+Douglas2022322@users.noreply.github.com>
2023-09-28 20:36:25 +0100

    Test BankAccount
    Created an example account using the constructor to test all methods.
    
    The code is working as it should be.
    
    No bugs fund it.

--- src/bankaccount/BankAccount.java
+++ src/bankaccount/BankAccount.java
@@ -15,6 +15,17 @@ public class BankAccount {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        BankAccountManagement account1 = new BankAccountManagement(100);
+        
+        System.out.println(account1.getBalance());
+        
+        account1.deposit(50);
+        
+        account1.withdraw(25.50);
+        
+        System.out.println(account1.getBalance());
+        
     }
     
 }
